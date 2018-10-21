9b0ddc5c42b95a7cb197fe96a67c8d8b53e216d6
David Bailey <davidbailey@users.noreply.github.com>
2018-10-21 18:57:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='RAIL',
-      version='0.3',
+      version='0.4',
       description='Risk Assessment Library',
       author='David Bailey',
       author_email='david@davidabailey.com',
