8f085a204b92d2f3b32c1775d9b11e1a1ee4ee5a
David Bailey <david@davidabailey.com>
2019-02-03 17:40:59 -0800

    added tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -2,6 +2,7 @@
 Tests for the Tree class
 """
 import unittest
+from unittest import mock
 
 from rail import Tree
 
@@ -27,6 +28,32 @@ class TestTree(unittest.TestCase):
         Test the path method of a Tree.
         """
         self.assertEqual(self.tree['test child'].path(), '/test tree/test child')
+
+    def test_print(self):
+        """
+        Test the to_print method of a Tree.
+        """
+        with mock.patch('builtins.print') as mock_print:
+            self.assertEqual(self.tree['test child'].to_print(), None)
+            mock_print.assert_called_once_with('/test tree/test child')
+            mock_print.reset_mock()
+            self.assertEqual(self.tree.to_print(), None)
+            mock_print.assert_called()
+            mock_print.assert_has_calls([mock.call('/test tree'), mock.call('/test tree/test child')])
+
+    def test_to_latex(self):
+        """
+        Test the to_latex method of a Tree.
+        """
+        with mock.patch('builtins.print') as mock_print:
+            self.assertEqual(self.tree.to_latex(), None)
+            mock_print.assert_called()
+            mock_print.assert_has_calls([
+                mock.call('child { node{test tree}'), 
+                mock.call('child { node{test child}'),
+                mock.call('}'),
+                mock.call('}')]
+            )   
 
     def test_to_dict_list(self):
         """
