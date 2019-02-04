0ca1d6af81295cdd2eb40afef28efd4d2d30abb2
David Bailey <david@davidabailey.com>
2019-02-03 16:41:17 -0800

    working on tests

--- rail/cpi.py
+++ rail/cpi.py
@@ -1,13 +1,26 @@
+"""
+A class to retrieve United States CPI data and calculate inflation
+"""
 import pandas as pd
 
-
-class CPI(object):
-    """A class to retrieve United States CPI data and calculate inflation"""
-
+class CPI:
+    """
+    A class to retrieve United States CPI data and calculate inflation
+    """
     def __init__(self) -> None:
-        self.cpi = pd.read_csv('https://download.bls.gov/pub/time.series/cu/cu.data.0.Current', sep='\t')
+        url = 'https://download.bls.gov/pub/time.series/cu/cu.data.0.Current'
+        self.cpi = pd.read_csv(url, sep='\t')
 
     def inflation(self, from_year: int, to_year: int) -> float:
-        cpi_from = self.cpi[(self.cpi['series_id'] == 'CUUS0000SA0      ') & (self.cpi['year'] == from_year) & (self.cpi['period'] == 'S01')]['value']
-        cpi_to = self.cpi[(self.cpi['series_id'] == 'CUUS0000SA0      ') & (self.cpi['year'] == to_year) & (self.cpi['period'] == 'S01')]['value']
+        """
+        A method to retrieve United States CPI data and calculate inflation
+        """
+        cpi_from = self.cpi[
+            (self.cpi['series_id'] == 'CUUS0000SA0      ') &
+            (self.cpi['year'] == from_year) & (self.cpi['period'] == 'S01')
+        ]['value']
+        cpi_to = self.cpi[
+            (self.cpi['series_id'] == 'CUUS0000SA0      ') &
+            (self.cpi['year'] == to_year) & (self.cpi['period'] == 'S01')
+        ]['value']
         return cpi_to.values[0] / cpi_from.values[0]
---
+++ tests/test_cpi.py
@@ -0,0 +1,23 @@
+"""
+Tests for the CPI class
+"""
+import unittest
+
+from rail import CPI
+
+class TestTree(unittest.TestCase):
+    """
+    Class to test a CPI.
+    """
+    def setUp(self):
+        self.cpi = CPI()
+
+    def test_inflation(self):
+        """
+        Test the inflation calculation method.
+        """
+        self.assertEqual(self.cpi.inflation(2010, 2010), 1.0)
+        self.assertEqual(self.cpi.inflation(2010, 2018), 1.1496494816926013)
+
+if __name__ == '__main__':
+    unittest.main()
